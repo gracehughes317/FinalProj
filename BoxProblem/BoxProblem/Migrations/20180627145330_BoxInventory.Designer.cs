4c503ffe64f3623c64efd963bf2628acb9508a2a
David Schulz <deschulz22@gmail.com>
2018-06-27 10:08:54 -0500

    Back End
    Created service and repository files, created migration, and created all methods in service and repo files.

--- BoxProblem/BoxProblem/Migrations/20180627145330_BoxInventory.Designer.cs
+++ BoxProblem/BoxProblem/Migrations/20180627145330_BoxInventory.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace BoxProblem.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180627145330_BoxInventory")]
+    partial class BoxInventory
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BoxProblem/BoxProblem/Services/BoxInventoryService.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BoxProblem.Data;
+using BoxProblem.Repositories;
+
+namespace BoxProblem.Services
+{
+    public class BoxInventoryService
+    {
+        private BoxInventoryRepository repository;
+
+        public BoxInventoryService(ApplicationDbContext context)
+        {
+            repository = new BoxInventoryRepository(context);
+        }
+
+        public List<BoxInventory> GetAllBoxInventorys()
+        {
+            return repository.GetAllBoxInventorys();
+        }
+
+        public void AddBoxInventory(BoxInventory toAdd)
+        {
+            repository.AddBoxInventory(toAdd);
+        }
+
+        public BoxInventory GetBoxInventoryById(int id)
+        {
+            return repository.GetBoxInventoryById(id);
+        }
+
+        public void SaveEdits(BoxInventory toSave)
+        {
+            repository.SaveEdits(toSave);
+        }
+
+        public void DeleteBoxInventory(BoxInventory toDelete)
+        {
+            repository.DeleteBoxInventory(toDelete);
+        }
+
+        public List<BoxInventory> FilterWeight(int weight)
+        {
+            return repository.FilterWeight(weight);
+        }
+
+        public List<BoxInventory> FilterVolume(int volume)
+        {
+            return repository.FilterVolume(volume);
+        }
+
+        public List<BoxInventory> FilterCanHoldLiquid(bool canHold)
+        {
+            return repository.FilterCanHoldLiquid(canHold);
+        }
+
+        public List<BoxInventory> FilterCost(double cost)
+        {
+            return repository.FilterCost(cost);
+        }
+
+        public List<BoxInventory> FilterCount(int count)
+        {
+            return repository.FilterCount(count);
+        }
+
+        public List<BoxInventory> FilterCreatedAt(DateTime date)
+        {
+            return repository.FilterCreatedAt(date);
+        }
+    }
+}
