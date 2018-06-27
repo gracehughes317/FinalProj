0372b4853cb44d96a9219ff63b2453bb3bec2a2e
Unknown <gracehughes317@gmail.com>
2018-06-27 10:55:55 -0500

    controller edited

--- BoxProblem/BoxProblem/Controllers/HomeController.cs
+++ BoxProblem/BoxProblem/Controllers/HomeController.cs
@@ -14,6 +14,10 @@ namespace BoxProblem.Controllers
         {
             return View();
         }
+        public IActionResult Add()
+        {
+            return View();
+        }
 
         public IActionResult About()
         {
