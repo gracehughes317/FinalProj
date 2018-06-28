89efe9da8018489f888e9f91f2e1e2422c007e96
Pranay Reddy <19reddyp@elmbrookstudents.org>
2018-06-28 13:25:21 -0500

    final

--- BoxProblem/BoxProblem/Controllers/HomeController.cs
+++ BoxProblem/BoxProblem/Controllers/HomeController.cs
@@ -7,18 +7,15 @@ using Microsoft.AspNetCore.Mvc;
 using BoxProblem.Models;
 using BoxProblem.Data;
 using BoxProblem.Services;
-
 namespace BoxProblem.Controllers
 {
     public class HomeController : Controller
     {
         private BoxInventoryService service;
-
         public HomeController(ApplicationDbContext context)
         {
             service = new BoxInventoryService(context);
         }
-
         public IActionResult Index(List<BoxInventory> model = null, bool doSearch = false)
         {
             if (!doSearch)
@@ -30,39 +27,30 @@ namespace BoxProblem.Controllers
                 return View(model);
             }
         }
-
-
-
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-
         public ActionResult Details(int id)
         {
             BoxInventory box = service.GetBoxInventoryById(id);
             return View(box);
         }
-
         public ActionResult Create()
         {
             return View();
         }
-
         [HttpPost]
         public ActionResult Create(BoxInventory toAdd)
         {
             if (ModelState.IsValid)
             {
                 toAdd.CreatedAt = DateTime.Now;
-
                 service.AddBoxInventory(toAdd);
                 return RedirectToAction("Index");
             }
-
             return View(toAdd);
         }
-
         [HttpPost]
         public ActionResult Index(string toSearch, string search)
         {
@@ -70,63 +58,10 @@ namespace BoxProblem.Controllers
             {
                 return View(service.FilterCount(Int32.Parse(toSearch)));
             }
-            else if (CostBox == true)
-            {
-                return View(service.FilterCost(toSearch));
-            }
-            else if (VolumeBox == true)
-            {
-                return View(service.FilterVolume((int)toSearch));
-            }
-            else if (WeightBox == true)
-            {
-                return View(service.FilterWeight((int)toSearch));
-            }
-            else
-            {
-                return View();
-            }
-        }
-
-        public ActionResult Edit(int id)
-        {
-            BoxInventory box = service.GetBoxInventoryById(id);
-            return View(box);
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(BoxInventory box)
-        {
-            if (ModelState.IsValid)
-            {
-                service.SaveEdits(box);
-                return RedirectToAction("Index");
-            }
-            return View(box);
-        }
-
-        public ActionResult Delete(int id)
-        {
-            BoxInventory box = service.GetBoxInventoryById(id);
-            return View(box);
-        }
-
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            BoxInventory box = service.GetBoxInventoryById(id);
-            service.DeleteBoxInventory(box);
-            return RedirectToAction("Index");
-        }
-    }
-}
-
-        
             else if (search.ToLower() == "costbox")
             {
                 return View(service.FilterCost(Double.Parse(toSearch)));
+            }
             else if (search.ToLower() == "volumebox")
             {
                 return View(service.FilterVolume(Int32.Parse(toSearch)));
@@ -141,4 +76,37 @@ namespace BoxProblem.Controllers
             }
             else
             {
-                return View();
+                return View();
+            }
+        }
+        public ActionResult Edit(int id)
+        {
+            BoxInventory box = service.GetBoxInventoryById(id);
+            return View(box);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(BoxInventory box)
+        {
+            if (ModelState.IsValid)
+            {
+                service.SaveEdits(box);
+                return RedirectToAction("Index");
+            }
+            return View(box);
+        }
+        public ActionResult Delete(int id)
+        {
+            BoxInventory box = service.GetBoxInventoryById(id);
+            return View(box);
+        }
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            BoxInventory box = service.GetBoxInventoryById(id);
+            service.DeleteBoxInventory(box);
+            return RedirectToAction("Index");
+        }
+    }
+}
