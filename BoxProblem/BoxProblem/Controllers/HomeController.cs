b928c364fcee7a1777aeb6d60ad718d70b1dcb09
Pranay Reddy <19reddyp@elmbrookstudents.org>
2018-06-28 12:04:33 -0500

    Edit done

--- BoxProblem/BoxProblem/Controllers/HomeController.cs
+++ BoxProblem/BoxProblem/Controllers/HomeController.cs
@@ -57,11 +57,26 @@ namespace BoxProblem.Controllers
         [HttpPost]
         public ActionResult Search(double toSearch, bool InventoryBox, bool CostBox, bool VolumeBox, bool WeightBox)
         {
-            if(InventoryBox == true)
+            if (InventoryBox == true)
             {
-                service.FilterCount((int)toSearch);
+                return View(service.FilterCount((int)toSearch));
             }
-
+            else if (CostBox == true)
+            {
+                return View(service.FilterCost(toSearch));
+            }
+            else if (VolumeBox == true)
+            {
+                return View(service.FilterVolume((int)toSearch));
+            }
+            else if (WeightBox == true)
+            {
+                return View(service.FilterWeight((int)toSearch));
+            }
+            else
+            {
+                return View();
+            }
         }
 
         public ActionResult Edit(int id)
@@ -99,31 +114,4 @@ namespace BoxProblem.Controllers
     }
 }
 
-        public ActionResult Search(double toSearch, bool InventoryBox, bool CostBox, bool VolumeBox, bool WeightBox)
-        {
-            if (InventoryBox == true)
-            {
-                return View(service.FilterCount((int)toSearch));
-            }
-            else if (CostBox == true)
-            {
-                return View(service.FilterCost(toSearch));
-            }
-            else if (VolumeBox == true)
-            {
-                return View(service.FilterVolume((int)toSearch));
-            }
-            else if (WeightBox == true)
-            {
-                return View(service.FilterWeight((int)toSearch));
-            }
-            else
-            {
-                return View();
-            }
-        }
-
-        public ActionResult Edit(int id)
-        {
-            BoxInventory box = service.GetBoxInventoryById(id);
-            return View(box);
+        
